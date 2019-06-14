4ddabe5c537346fc5299037c6040a3a5484d782b
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2019-06-14 15:33:27 +0300

    Add files via upload

---
+++ GenericsLab/JarOfT/Main.java
@@ -0,0 +1,17 @@
+package GenericsLab.JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<String> jar = new Jar<>();
+
+        jar.add("Pesho");
+        jar.add("Pesho1");
+        jar.add("Pesho2");
+        jar.add("Pesho3");
+
+        System.out.println(jar.remove());
+
+
+    }
+}
