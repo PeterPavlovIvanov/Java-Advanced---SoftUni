2c14482a70fa7c26806983f06f1ef24f9d87f6c4
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2019-06-14 15:36:20 +0300

    Add files via upload

---
+++ GenericsLab/ListUtilities/Main.java
@@ -0,0 +1,17 @@
+package GenericsLab.ListUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+// test ListUtil here
+//        List<Integer> a = new ArrayList<>();
+//
+//        a.add(1);
+//        a.add(132);
+//        a.add(4321);
+//        a.add(14);
+//        a.add(71);
+//        a.add(-1);
+//        System.out.println(GenericsLab.ListUtilities.ListUtils.getMin(a));
+//        System.out.println(GenericsLab.ListUtilities.ListUtils.getMax(a));
+    }
+}
