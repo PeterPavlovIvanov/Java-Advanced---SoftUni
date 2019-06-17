75654b0dc51f025bf9657ff7cf549233b835320c
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2019-06-17 17:37:57 +0300

    Add files via upload

---
+++ GenericsExercise/GenericsBox/Box.java
@@ -0,0 +1,15 @@
+package GenericsExercise.GenericsBox;
+
+public class Box<T> {
+    private T item;
+
+    public Box(T item) {
+        this.item = item;
+    }
+
+    @Override
+    public String toString() {
+        return this.item.getClass().getName()+
+                ": " + this.item;
+    }
+}
